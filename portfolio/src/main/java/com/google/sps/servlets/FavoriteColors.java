f50425b09bc1e4cf54f7dd7e8616b75d44812c2c
Carine Gordillo <carinegordillo@gmail.com>
2021-03-07 21:12:17 +0000

    Fixed spacing

--- portfolio/src/main/java/com/google/sps/servlets/FavoriteColors.java
+++ portfolio/src/main/java/com/google/sps/servlets/FavoriteColors.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 public class FavoriteColors extends HttpServlet {
 
   @Override
-
-  
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     ArrayList<String> colors = new ArrayList<String>();
     colors.add("1. Blue ");
